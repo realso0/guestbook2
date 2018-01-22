0eaedc869bc2a821550a0a63139806405b7ed1bf
BIT-USER <BIT-USER@DESKTOP-GO4E25Q>
2018-01-22 17:23:30 +0900

    01/22

--- src/com/javaex/controller/guestbookServlet.java
+++ src/com/javaex/controller/guestbookServlet.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.javaex.dao.GuestbookDao;
-import com.javaex.vo.EmaillistVo;
 import com.javaex.vo.GuestbookVo;
 
 
