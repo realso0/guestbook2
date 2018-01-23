86742b4324d553f6fbdfbab8b47a7b8d21ab4119
BIT-USER <BIT-USER@DESKTOP-GO4E25Q>
2018-01-23 09:06:35 +0900

    01/23

--- src/com/javaex/controller/guestbookController.java
+++ src/com/javaex/controller/guestbookController.java
@@ -20,7 +20,7 @@ public class guestbookController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//System.out.println("환영합니다 Servlet guestbook2"); //웹페이지와 연결됬는지 확인
 		request.setCharacterEncoding("utf-8");
-		String actionform=request.getParameter("a"); //입력받는 값으로, a 라는 변수를 하나 만들어 줌.
+		String actionform=request.getParameter("a"); //주소 창에 입력받는 값은 값으로 a를 하나 만들어줌. 요청문서에서 a를 얻어옴.
 		
 		if ("list".equals(actionform)) {
 			System.out.println("list 진입");
@@ -34,17 +34,17 @@ public class guestbookController extends HttpServlet {
 			RequestDispatcher rd=request.getRequestDispatcher("list.jsp"); //포워드 작업
 			rd.forward(request,response);
 		} else if ("deleteform".equals(actionform)) {
-			//System.out.println("deleteform 진입"); //여기까지 웹 접속되는지 확인
+			System.out.println("deleteform 진입"); //여기까지 웹 접속되는지 확인
 			
 			int no = Integer.parseInt(request.getParameter("no"));
 			request.setAttribute("no", no);
 			
 			RequestDispatcher rd=request.getRequestDispatcher("deleteform.jsp"); //값을 입력받아, RequestDispatcher객체가 만들어지고, 
-			//rd로 객체를 받는다. request클래스 내의 getRequestDispatcher메소드를 이용해, form.jsp로 보낸다.
-			rd.forward(request, response); //rd의 forword메소드를 이용하여, request문서와 response문서를 form.jsp로 보낸다.
+			//rd로 객체를 받는다. request클래스 내의 getRequestDispatcher메소드를 이용해, deleteform.jsp로 보낸다.
+			rd.forward(request, response); //rd의 forword메소드를 이용하여, request문서와 response문서를 deleteform.jsp로 보낸다.
 			
 		} else if ("add".equals(actionform)) {
-			//System.out.println("add 진입");
+			System.out.println("add 진입");
 			
 			String name=request.getParameter("name"); //폼에서 약속한 변수이름의 값을 받아옴.
 			String password=request.getParameter("pass");
